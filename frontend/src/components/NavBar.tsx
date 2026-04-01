bedd5fe5651741da6611c03e06283e105d068f66
sabijnas <sabinah97@hotmail.com>
2026-04-01 11:37:22 +0200

    responsive nav

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,15 +1,25 @@
 import { Link } from "react-router-dom";
 import "../css/NavBar.css";
+import { useState } from "react";
 
 const NavBar = () => {
+  /* isOpen = om menyn är öppen true/false 
+     setIsOpen = funktion för att ändra värdet */
+  const [isOpen, setIsOpen] = useState(false)
   return (
     <nav className="navbar">
       <p className="nav-logo">VetPrep</p>
-      <div className="nav-links">
-        <Link to="/">Hem</Link>
-        <Link to="/Affärsplan">Affärsplan</Link>
-        <Link to="/Projektidé">Projektidé</Link>
-        <Link to="/LäsMer">Läs Mer</Link>
+      
+      {/* Vid klick växlas true/false */}
+      <div className="hamburger-menu" onClick={() => setIsOpen(!isOpen)}>≡</div>
+      
+      {/* isOpen = true, lägg till active (menyn visas på mindre skärmar)
+          om false = nav-links visas */}
+      <div className={isOpen ? "nav-links active" : "nav-links"}>
+        <Link to="/" onClick={() => setIsOpen(false)}>Hem</Link>
+        <Link to="/Affärsplan" onClick={() => setIsOpen(false)}>Affärsplan</Link>
+        <Link to="/Projektidé" onClick={() => setIsOpen(false)}>Projektidé</Link>
+        <Link to="/LäsMer" onClick={() => setIsOpen(false)}>Läs Mer</Link>
       </div>
     </nav>
   );
