cab7ffca624a178f7f78bd473b852f2cec75c199
sabijnas <sabinah97@hotmail.com>
2026-05-06 12:30:54 +0200

    terms update

--- frontend/src/components/Terms.tsx
+++ frontend/src/components/Terms.tsx
@@ -9,17 +9,17 @@ return (
 
         <h2 className="headerTearms">Användarvillkor</h2>
 
-        <p className="Tearms-form"> 
+        <div className="tearms-form"> 
             Dessa användarvillkor gäller för användningen av VetPrep, genom att skapa ett konto och använda applikationen godkänner du villkoren nedan. Applikationen är ett skolprojekt och tillhandahålls i befintligt skick utan garanti för funktion, tillgänglighet eller korrekthet.
+            <br /> <br />
+            ● Applikationen är inte en medicinsk tjänst och ersätter inte professionell veterinärvård. All information i VetPrep är endast avsedd som stöd för dig som djurägare.
             <br />
-            - Applikationen är inte en medicinsk tjänst och ersätter inte professionell veterinärvård. All information i VetPrep är endast avsedd som stöd för dig som djurägare.
+            ● Applikationen är avsedd för att logga symtom, spara information och exportera loggar till PDF.
             <br />
-            - Applikationen är avsedd för att logga symtom, spara information och exportera loggar till PDF.
+            ● Applikationen samlar in följande personuppgifter: namn, e-post, lösenord, djurets namn, hälsologgar. Ingen information delas med tredje part.
             <br />
-            - Applikationen samlar in följande personuppgifter: Namn, E-post, Lösenord, Djurets namn, Hälsologgar. Ingen information delas med tredje part.
-            <br />
-            - Vid misstanke om sjukdom eller akuta symtom ska du alltid i första hand kontakta en legitermad veterinär.
-        </p>
+            ● Vid misstanke om sjukdom eller akuta symtom ska du alltid i första hand kontakta en legitermad veterinär.
+        </div>
         <button className="closeTermsBtn" onClick={onClose}>Stäng</button>
    </div> </div>
 )
